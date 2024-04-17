deb04691beab175ed1206b2c96f841335c0a0d30
unknown <madhusagardasari@gmail.com>
2024-04-18 00:31:44 +0800

    username modified

---
+++ 04-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+### declaring the list of array
+DAYS=("sunday" "monday" "twesday" "thrusday" "friday" "saturday")
+#####size of array starts with 1
+###here array size is 6
+###index of array start with 0
+####here index of array is 5
+echo "please enter week value:"
+read DAY
+echo "entered day is: {$DAY}"
+
