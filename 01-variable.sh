9f058d4e2e4a7d34a1fa0ded5b11405fec438f3d
unknown <madhusagardasari@gmail.com>
2024-04-17 17:51:35 +0800

    my first commit

--- 01-variable.sh
+++ 01-variable.sh
@@ -5,8 +5,8 @@ PERSON2=vishruth
 
 ###refering the variable
 
-echo "$PERSON1:hello $PERSON2"
-echo "$PERSON2:hi $PERSON1"
-echo "$PERSON1:how r u?"
-echo "$PERSON2:i am good how about u?"
-echo "$PERSON1:i am being fine"
+echo "$PERSON1 :hello $PERSON2"
+echo "$PERSON2 :hi $PERSON1"
+echo "$PERSON1 :how r u?"
+echo "$PERSON2 :i am good how about u?"
+echo "$PERSON1 :i am being fine"
