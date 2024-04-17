3f63db821caaffaf5654d6fa0e6c5611785bae71
unknown <madhusagardasari@gmail.com>
2024-04-17 17:44:02 +0800

    my first commit

---
+++ 01-variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+### variable declaration
+PERSON1=madhu
+PERSON2=shiva
+
+###refering the variable
+
+echo "$PERSON1:hello $PERSON2"
+echo "$PERSON2:hi $PERSON1"
+echo "$PERSON1:how r u?"
+echo "$PERSON2:i am good how about u?"
+echo "$PERSON1:i am being fine"
---
+++ 02-variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+### passing arguments/parameters
+PERSON1=$1
+PERSON2=$2
+
+###refering the variable
+
+echo "$PERSON1:hello $PERSON2"
+echo "$PERSON2:hi $PERSON1"
+echo "$PERSON1:how r u?"
+echo "$PERSON2:i am good how about u?"
+echo "$PERSON1:i am being fine"
