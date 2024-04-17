fc2642eb0283f4a61b658fca0f8b4cb079b15299
unknown <madhusagardasari@gmail.com>
2024-04-18 00:49:24 +0800

    data types

--- 05-datatypes.sh
+++ 05-datatypes.sh
@@ -5,7 +5,7 @@ NUM1=$1
 NUM2=$2
 ### refering arguments and adding them
 
-sum=$(($NUM1+$NUM2))
+SUM=$(($NUM1+$NUM2))
 ####we can use operators for algarithm
 
 echo "sum of $NUM1 and $NUM2 is:$SUM"
