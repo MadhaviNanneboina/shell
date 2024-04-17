1d46d157b14df8d135125e939fca90d7f01e824b
unknown <madhusagardasari@gmail.com>
2024-04-17 17:55:48 +0800

    my first commit

--- variable.sh
+++ variable.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #### static code
-echo "madhu:hello shiva"
-echo "shiva:hi madhu"
-echo "madhu:how r u?"
-echo "shiva:i am good how about u?"
-echo "madhu:i am being fine"
+##echo "madhu:hello shiva"
+##echo "shiva:hi madhu"
+##echo "madhu:how r u?"
+##echo "shiva:i am good how about u?"
+##echo "madhu:i am being fine"
+echo "hello world"
