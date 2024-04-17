e4eeb5000fd664dd79ae1cfa12c07cfe9cb4201e
unknown <madhusagardasari@gmail.com>
2024-04-18 01:31:23 +0800

    special variables1

--- 06-special-variables.sh
+++ 06-special-variables.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 echo "All variables:$@"
+echo "to know number of variables passed:$#"
+echo "to know home directory of current user:$HOME"
+echo "to know present working directory:$PWD"
+echo "to know script name: $0"
+echo "to know which user is running this script:$USER"
+echo "to know host name:$HOSTNAME"
+echo "to know process id of current shell script:$$"
+ ##sleep 60 #### the script will run intil 60 sec afterthat it will exit
+###to come out from sleep mode press & it will come back from sleep
+echo "to know process id of last background command:$!"
+
