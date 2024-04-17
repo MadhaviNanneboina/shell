cf43e1e20803e27dea4ead49df2fea2c9dca6e77
unknown <madhusagardasari@gmail.com>
2024-04-18 00:02:03 +0800

    username modified

--- 03-variable.sh
+++ 03-variable.sh
@@ -2,9 +2,9 @@
 ## displaying username and password
 echo "please enter username:"
 read -s USERNAME
-echo "username:$USERNAME"
+
 echo "please enter password:"
 read -s PASSWORD
-echo "password:$PASSWORD"
+
 echo "username is:$USERNAME"
 echo "password is:$PASSWORD"
