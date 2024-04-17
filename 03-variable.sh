58cb3f0d280b39328e0c79f6b76d7af1522ed96a
unknown <madhusagardasari@gmail.com>
2024-04-17 23:56:38 +0800

    username,password displaying

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,1 +1,10 @@
-echo "i am madhavi"
+#!/bin/bash
+## displaying username and password
+echo "please enter username:"
+read -s USERNAME
+echo "username:$USERNAME"
+echo "please enter password:"
+read -s PASSWORD
+echo "password:$PASSWORD"
+echo "username is:$USERNAME"
+echo "password is:$PASSWORD"
