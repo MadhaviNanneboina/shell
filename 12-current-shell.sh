c9a64ba7d98ba1d80ce494a4e1ab1b4390005a8b
unknown <madhusagardasari@gmail.com>
2024-04-24 13:28:52 +0800

    comiting source

--- 12-current-shell.sh
+++ 12-current-shell.sh
@@ -4,5 +4,10 @@ USER="this is madhavi i am devops engineer"
 echo "before calling other script current script:$USER"
 echo "process id of current script:$$"
 
-./13-other-shell.sh
+#calling other script from our script 
+#by using ./
+#./13-other-shell.sh
+
+#by using source
+source ./13-other-shell.sh
 echo "after calling other script:$USER"
