7690db228c8c936c1ebda76374842f72a673a659
unknown <madhusagardasari@gmail.com>
2024-04-28 01:09:11 +0800

    zipping old logs

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 disk_usage=$(df -hT | grep xfs)
 disk_theshould=5
+message=""
 
 while IFS= read -r line
 do
@@ -8,6 +9,8 @@ usage=$(echo $line | awk -F " " '{print $6}' | cut -d "%" -f1)
 folder=$(echo $line |awk -F " " '{print $NF}')
 if [ $usage -ge $disk_theshould ]
 then
-    echo "$folder is more than $disk_theshould , current usage is $usage"
+#to append results will use + symbol to avoid override
+    message+="$folder is more than $disk_theshould , current usage is $usage"
 fi
 done <<< $disk_usage
+echo "message is :$message"
